c2cbde390eb619803c1fafe9a9857f5920ce018f
Martin Sieniawski <m.sieniawski@live.ca>
2015-03-18 17:06:20 -0400

    Added range and vilidity checks to generator

--- src/generator/PuzzleGenerator.java
+++ src/generator/PuzzleGenerator.java
@@ -12,18 +12,33 @@ public class PuzzleGenerator {
     public int[][] generate() {
         Random rand = new Random();
         int[][] grid = new int[10][10];
-        int x, y, ship = 0;
+        int x, y, ship, endpoint = 0;
+        String orientation = "";
+        boolean placed = false;
         
         // Randomly add ships to grid
         for (int i = 0; i < 10; i++) {
-            boolean placed = false;
+            ship = SHIP_TYPES[i];
+            placed = false;
             while (!placed) {
-                x = rand.nextInt(9);
-                y = rand.nextInt(9);
+                x = rand.nextInt(10);
+                y = rand.nextInt(10);
+                orientation = ORIENTATIONS[rand.nextInt(2)];
                 
-                if (grid[x][y] == 0) {
-                    ship = SHIP_TYPES[i];
-                    grid[x][y] = ship;
+                if (isPlacementValid(x, y, orientation, ship, grid)) {
+                    if (orientation.equals("vertical")) {
+                        // Place ship vertically
+                        endpoint = y + ship;
+                        for (int j = y; j < endpoint; j++) {
+                            grid[x][j] = ship;
+                        }
+                    } else {
+                        // Place ship horizontally
+                        endpoint = x + ship;
+                        for (int j = x; j < endpoint; j++) {
+                            grid[j][y] = ship;
+                        }
+                    }
                     placed = true;
                 }
             }
@@ -31,13 +46,36 @@ public class PuzzleGenerator {
         return grid;
     }
     
+    public boolean isPlacementValid(int x, int y, String orientation, int ship, int[][] grid) {
+        int endpoint = 0;
+        if (orientation.equals("vertical")) {
+            // Check vertically
+            endpoint = y + ship;
+            for (int i = y; i < endpoint; i++) {
+                if (i >= grid.length || grid[x][i] != 0) {
+                    return false;
+                }
+            }
+        } else {
+            // Check horizontally
+            endpoint = x + ship;
+            for (int i = x; i < endpoint; i++) {
+                if (i >= grid.length || grid[i][y] != 0) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
     public void printGrid(int[][] grid) {
         int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = grid[0].length - 1; i >= 0; i--) {
+            for (int j = 0; j < grid.length; j++) {
                 if (grid[i][j] != 0)
                     count++;
-                System.out.print(grid[i][j] + " ");
+                System.out.print(grid[j][i] + " ");
             }
             System.out.println();
         }
@@ -49,7 +87,7 @@ public class PuzzleGenerator {
     public static void main(String[] args) {
         PuzzleGenerator p = new PuzzleGenerator();
         
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 1; i++) {
             p.printGrid(p.generate());
             System.out.println();
         }
