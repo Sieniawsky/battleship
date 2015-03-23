316eef3c1cd63f920bbb51ef84f59868cc15f5bc
Martin Sieniawski <m.sieniawski@live.ca>
2015-03-23 16:57:59 -0400

    Added basic loader implementation

--- src/core/Loader.java
+++ src/core/Loader.java
@@ -1,22 +1,65 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Loader {
     // Loads a data-set and parses lines, marshaling each into
     // an instance of Puzzle. The instance is then fed to Runner
+    int index;
+    int size;
+    List<String> dataset;
     
-    public Loader() {}
+    public Loader() {
+        index = 0;
+    }
     
-    public void load(String filename) {}
+    public boolean load(String filename) {
+        
+        try {
+            FileReader reader = new FileReader(filename);
+            BufferedReader bfr = new BufferedReader(reader);
+            dataset = new ArrayList<String>();
+            String line = null;
+            while ((line = bfr.readLine()) != null) {
+                dataset.add(line);
+            }
+            bfr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+        size = dataset.size();
+        
+        return false;
+    }
     
     public Puzzle decode(String encoded) {
         return null;
     }
     
     public boolean hasNext() {
-        return false;
+        return (index + 1) < size;
     }
     
     public Puzzle getNext() {
-        return null;
+        
+        if (index + 1 >= size) {
+            return null;
+        }
+        
+        Puzzle instance = decode(dataset.get(index));
+        index++;
+        
+        return instance;
     }
 }
