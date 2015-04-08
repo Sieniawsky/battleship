ce3175efe0e63060ab21ac3403fd0367c10fdf8e
Martin Sieniawski <m.sieniawski@live.ca>
2015-04-08 15:20:15 -0400

    Added some comments

--- src/core/GeneticSolver.java
+++ src/core/GeneticSolver.java
@@ -1,13 +1,19 @@
 package core;
 
-public class GeneticSolver extends Solver
-{
+public class GeneticSolver extends Solver {
 
 	@Override
-	protected int run(Puzzle puzzle)
-	{
-		// TODO Auto-generated method stub
-		return 0;
+	protected int run(Puzzle puzzle) {
+	    int operations = 0;
+	    
+	    // Step 1. Randomly place ships on the grid
+	    
+	    // Step 2. Compute solution fitness
+	    
+	    // Step 3. Remove the worst scoring grids and generate
+	    //         mutated replacement grids
+	    
+		return operations;
 	}
 
 }
